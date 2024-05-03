644fa6948439a89f86f42c3efd13b47205598e1c
“Crystal <hcheung109@my.bcit.ca>
2024-05-03 14:46:23 -0700

    add custom cursor

---
+++ script/site.js
@@ -0,0 +1,17 @@
+const cursorGraphic = document.querySelector('.cursor');
+
+const moveImageWithCursor = (e) => {
+  const mouseX = e.clientX;
+  const mouseY = e.clientY;
+
+  const offsetX = 20; 
+  const offsetY = 20; 
+
+  const newImageX = mouseX + offsetX;
+  const newImageY = mouseY + offsetY;
+
+  cursorGraphic.style.left = `${newImageX}px`;
+  cursorGraphic.style.top = `${newImageY}px`;
+};
+
+window.addEventListener('mousemove', moveImageWithCursor);
