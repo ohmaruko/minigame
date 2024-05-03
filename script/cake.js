78229a242eb841219abc5154a197116d0eb0b5fd
madoka <mnogaki@my.bcit.ca>
2024-05-03 14:08:30 -0700

    add functionality on change background button

--- script/cake.js
+++ script/cake.js
@@ -1,1 +1,8 @@
-document.getElementById("test").style.color = "green";
+// Background
+document.getElementById("background").style.backgroundColor = "#F5D2CC";
+document.getElementById("bgbtn").addEventListener("click", changeBgColor);
+function changeBgColor() {
+    const colors = ["#F5D2CC", "#97D5CD", "#D5B197", "#D5CB97", "#97D599", "#979DD5", "#C197D5", "#D597CB", "#D597A6"]
+    let selectedColor = colors[Math.floor(Math.random() * colors.length - 1)];
+    document.getElementById("background").style.backgroundColor = selectedColor;
+}
