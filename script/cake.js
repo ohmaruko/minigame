fd81f34a28155985b14c386ecb28dae09326a219
madoka <mnogaki@my.bcit.ca>
2024-05-14 12:23:55 -0700

    organize codes

--- script/cake.js
+++ script/cake.js
@@ -16,3 +16,42 @@ document.addEventListener('DOMContentLoaded', () => {
         window.history.back();
     });
 });
+
+// Display of the cake
+function showElement(type, name) {
+
+    const elementToShow = document.querySelector(`#${type}-${name}`);
+    
+    // Toggle the display state of the clicked element
+    if (elementToShow.style.display === 'none') {
+        elementToShow.style.display = 'block';
+    } else {
+        elementToShow.style.display = 'none';
+    }
+    
+    // Show the clicked element and hide all others
+    const elements = document.querySelectorAll(`.${type}Img`);
+    elements.forEach(element => {
+        if (element.id === `${type}-${name}`) {
+            element.style.display = 'block';
+        } else {
+            element.style.display = 'none';
+        }
+    });
+
+}
+
+
+function toggleToppingDisplay(name) {
+const topping = document.querySelector(`#topping-${name}`);
+if (topping) {
+    topping.style.display = topping.style.display === 'none' ? 'block' : 'none';
+}
+}
+
+// Name input
+function changeDisplayName() {
+const newName = document.getElementById('inputName').value;
+const displayName = document.getElementById('displayName');
+displayName.textContent = newName;
+}
--- script/site.js
+++ script/site.js
@@ -1,3 +1,4 @@
+// Custom Cursor
 const cursorGraphic = document.querySelector('.cursor');
 
 const moveImageWithCursor = (e) => {
@@ -22,3 +23,35 @@ window.addEventListener('scroll', () => {
     clientY: event.clientY
   });
 });
+
+
+// Display of the cake
+var cakeLayer = ["./images/cream_white.svg", "./images/cream_chocolate.svg"];
+var cakeToppings = ["./images/berries.svg", "./images/chocolates.svg"];
+
+// To track which image layer to show
+var currentCakeLayerIndex = 0;
+// To track which image layer to show
+var currentCakeToppingIndex = 0;
+
+function changeLayer() {
+    var cakeLayerImage = document.getElementById("cakeLayer");
+    cakeLayerImage.src = cakeLayer[currentCakeLayerIndex];
+    currentCakeLayerIndex = (currentCakeLayerIndex + 1) % cakeLayer.length;
+}
+
+function changeTopping() {
+    var cakeToppingImage = document.getElementById("cakeTopping");
+    cakeToppingImage.src = cakeToppings[currentCakeToppingIndex];
+    
+    cakeToppingImage.width = 350; 
+    cakeToppingImage.height = 200;
+
+    currentCakeToppingIndex = (currentCakeToppingIndex + 1) % cakeToppings.length;
+}
+
+changeLayer();
+changeTopping();
+
+setInterval(changeLayer, 3000);
+setInterval(changeTopping, 3000);
